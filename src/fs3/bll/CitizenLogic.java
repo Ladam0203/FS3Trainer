fc644f85d00f18623c94d8c78dc98a43fa0c04db
Miloš Jozek <90444097+BoldHeadBall@users.noreply.github.com>
2022-05-02 10:59:00 +0200

    Renamed dao interface
    + plus implemented some methods in citizen logic

--- src/fs3/bll/CitizenLogic.java
+++ src/fs3/bll/CitizenLogic.java
@@ -1,4 +1,18 @@
 package fs3.bll;
 
+import fs3.be.Citizen;
+import fs3.dal.CitizenDAO;
+import fs3.dal.DAO;
+
+import java.util.List;
+import java.util.Optional;
+
 public class CitizenLogic {
+   private DAO<Citizen> citizenDAO;
+
+   public CitizenLogic(){ citizenDAO = new CitizenDAO();}
+
+    public Optional<Citizen> getCitizen(int id){ return citizenDAO.read(id);}
+
+    public List<Citizen> getAllCitizens(){return citizenDAO.readAll();}
 }
--- src/fs3/dal/DAO.java
+++ src/fs3/dal/DAO.java
@@ -1,5 +1,6 @@
 package fs3.dal;
 
+import com.microsoft.sqlserver.jdbc.SQLServerException;
 import fs3.be.Citizen;
 
 import java.sql.ResultSet;
@@ -7,13 +8,16 @@ import java.util.List;
 import java.util.Optional;
 
 public interface DAO<T> {
-    Optional<T> get(int id) throws Exception;
 
-    List<T> getAll() throws Exception;
 
-    void save(T t) throws Exception;
+    Optional<T> read(int id);
+
+    List<T> readAll() throws Exception;
+
+    Optional<T> create(T t);
 
     void update(T t, String[] params) throws Exception;
+
 
     void delete(T t) throws Exception;
 
