7062db6ff4f1ab7a34185af033a8c17bbb20e02f
RomanMeasv <90683512+RomanMeasv@users.noreply.github.com>
2022-05-03 11:40:09 +0200

    UI designing & file structuring

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("fs3/gui/view/StudentPageViewNew.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("fs3/gui/view/student/StudentPageViewNew.fxml"));
 
         Scene scene = new Scene(root);
 
