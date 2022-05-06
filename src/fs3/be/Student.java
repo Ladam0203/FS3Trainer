cb828f1b558f12293c56bd3c2cf27bbaebf21539
Ladam0203 <adam7605@easv365.dk>
2022-05-06 10:51:11 +0200

    Modified user and student be

--- src/fs3/be/Student.java
+++ src/fs3/be/Student.java
@@ -1,15 +1,8 @@
 package fs3.be;
 
 public class Student extends  User {
-    public Student(String username, String password){
-        setUsername(username);
-        setPassword(password);
-    }
-
-    public Student(int id, String username, String password){
-        setId(id);
-        setUsername(password);
-        setPassword(password);
+    public Student(String username, String password) {
+        super(username, password);
     }
 
     @Override
--- src/fs3/be/User.java
+++ src/fs3/be/User.java
@@ -1,31 +1,54 @@
 package fs3.be;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public abstract class User {
-    protected int id;
-    protected String username;
-    protected String password;
+    protected IntegerProperty id;
+    protected StringProperty username;
+    protected StringProperty password;
+
+    public User(String username, String password) {
+        this.id = new SimpleIntegerProperty();
+        this.username = new SimpleStringProperty();
+        this.password = new SimpleStringProperty();
+    }
 
     public int getId() {
+        return id.get();
+    }
+
+    public IntegerProperty idProperty() {
         return id;
     }
 
     public void setId(int id) {
-        this.id = id;
+        this.id.set(id);
     }
 
     public String getUsername() {
+        return username.get();
+    }
+
+    public StringProperty usernameProperty() {
         return username;
     }
 
     public void setUsername(String username) {
-        this.username = username;
+        this.username.set(username);
     }
 
     public String getPassword() {
+        return password.get();
+    }
+
+    public StringProperty passwordProperty() {
         return password;
     }
 
     public void setPassword(String password) {
-        this.password = password;
+        this.password.set(password);
     }
 }
