e459067c690708d3e6650b1706a5f490586d1d54
Ladam0203 <adam7605@easv365.dk>
2022-05-07 10:33:19 +0200

    CitizenDAO threading on readAll

--- src/fs3/dal/citizen/HealthConditionDAO.java
+++ src/fs3/dal/citizen/HealthConditionDAO.java
@@ -11,7 +11,6 @@ import fs3.enums.HealthConditionState;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.time.LocalDate;
 import java.util.*;
 
 public class HealthConditionDAO {
