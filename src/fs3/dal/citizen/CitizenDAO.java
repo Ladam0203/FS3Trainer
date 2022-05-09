f2c8e90336e6e06d31ca1109309ba0c29c7ce720
Ladam0203 <adam7605@easv365.dk>
2022-05-09 13:33:59 +0200

    Multithreading process in DAO

--- src/fs3/dal/citizen/CitizenDAO.java
+++ src/fs3/dal/citizen/CitizenDAO.java
@@ -4,6 +4,9 @@ import fs3.be.Citizen;
 import fs3.dal.ConnectionManager;
 import fs3.dal.ConnectionManagerPool;
 
+import javax.sql.RowSet;
+import javax.sql.rowset.CachedRowSet;
+import javax.sql.rowset.RowSetProvider;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -18,8 +21,8 @@ public class CitizenDAO {
     private final HealthConditionDAO healthConditionDAO = new HealthConditionDAO();
     private final FunctionalAbilityDAO functionalAbilityDAO = new FunctionalAbilityDAO();
 
-    ExecutorService subExecutor = Executors.newFixedThreadPool(4);
-    ExecutorService executor = Executors.newCachedThreadPool();
+    ExecutorService executor;
+    ExecutorService subExecutor;
 
     String tableName = "Citizens";
     String[] columns = {"id"};
@@ -35,13 +38,21 @@ public class CitizenDAO {
         try (Connection con = cm.getConnection()) {
             PreparedStatement ps = con.prepareStatement(readAll);
             ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                executeAsync(new ExceptionCallable() {
-                    @Override
-                    void doTask() throws Exception {
-                        citizens.add(constructCitizen(rs));
-                    }
-                });
+
+            CachedRowSet crs = RowSetProvider.newFactory().createCachedRowSet();
+            crs.populate(rs);
+
+            List<Future<Exception>> futures = new ArrayList<>();
+            executor = Executors.newCachedThreadPool();
+            while (crs.next()) {
+                citizens.add(constructCitizen(crs));
+            }
+
+            executor.shutdown();
+            for (Future<Exception> future : futures) {
+                if (future.get() != null) {
+                    throw future.get();
+                }
             }
         } finally {
             ConnectionManagerPool.getInstance().returnConnectionManager(cm);
@@ -56,25 +67,25 @@ public class CitizenDAO {
     public void update(Citizen citizen) throws Exception {
         subExecutor = Executors.newFixedThreadPool(4);
         List<Future<Exception>> futures = new ArrayList<>();
-        futures.add(executor.submit(new ExceptionCallable() {
+        futures.add(subExecutor.submit(new ExceptionCallable() {
             @Override
             void doTask() throws Exception {
                 personalInformationDAO.update(citizen);
             }
         }));
-        futures.add(executor.submit(new ExceptionCallable() {
+        futures.add(subExecutor.submit(new ExceptionCallable() {
             @Override
             void doTask() throws Exception {
                 generalInformationDAO.update(citizen);
             }
         }));
-        futures.add(executor.submit(new ExceptionCallable() {
+        futures.add(subExecutor.submit(new ExceptionCallable() {
             @Override
             void doTask() throws Exception {
                 healthConditionDAO.update(citizen);
             }
         }));
-        futures.add(executor.submit(new ExceptionCallable() {
+        futures.add(subExecutor.submit(new ExceptionCallable() {
             @Override
             void doTask() throws Exception {
                 functionalAbilityDAO.update(citizen);
@@ -90,31 +101,33 @@ public class CitizenDAO {
     }
 
     private Citizen constructCitizen(ResultSet rs) throws Exception {
+        int pos = rs.getRow();
+        System.out.println("pos: " + pos);
         Citizen citizen = new Citizen();
         int citizenId = rs.getInt(columns[0]);
         citizen.setId(citizenId);
 
         subExecutor = Executors.newFixedThreadPool(4);
         List<Future<Exception>> futures = new ArrayList<>();
-        futures.add(executor.submit(new ExceptionCallable() {
+        futures.add(subExecutor.submit(new ExceptionCallable() {
             @Override
             void doTask() throws Exception {
                 citizen.setPersonalInformation(personalInformationDAO.read(citizen));
             }
         }));
-        futures.add(executor.submit(new ExceptionCallable() {
+        futures.add(subExecutor.submit(new ExceptionCallable() {
             @Override
             void doTask() throws Exception {
                 citizen.setGeneralInformation(generalInformationDAO.read(citizen));
             }
         }));
-        futures.add(executor.submit(new ExceptionCallable() {
+        futures.add(subExecutor.submit(new ExceptionCallable() {
             @Override
             void doTask() throws Exception {
                 citizen.setHealthConditions(healthConditionDAO.read(citizen));
             }
         }));
-        futures.add(executor.submit(new ExceptionCallable() {
+        futures.add(subExecutor.submit(new ExceptionCallable() {
             @Override
             void doTask() throws Exception {
                 citizen.setFunctionalAbilities(functionalAbilityDAO.read(citizen));
@@ -134,10 +147,4 @@ public class CitizenDAO {
     /*
     * Submits task to executor service and throws the exception upwards if it happened, not really async because it will wait to finish...
      */
-    public void executeAsync(ExceptionCallable callable) throws Exception {
-        Future<Exception> exception = executor.submit(callable);
-        if (exception.get() != null) {
-            throw exception.get();
-        }
-    }
 }
