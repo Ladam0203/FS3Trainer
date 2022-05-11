7611945b22c08cc80d0da3e24ebba66c6e64e11c
RomanMeasv <90683512+RomanMeasv@users.noreply.github.com>
2022-05-11 09:12:31 +0200

    Base for teacher page

---
+++ src/fs3/gui/controller/teacher/TeacherPageController.java
@@ -0,0 +1,14 @@
+package fs3.gui.controller.teacher;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class TeacherPageController {
+    public ListView ltvCitizenTemplates;
+    public TextField txfTemplateName;
+
+    public void handleAddTemplate(ActionEvent event) {
+        
+    }
+}
