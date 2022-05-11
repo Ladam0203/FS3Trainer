f395cb668d579da041ef509a30eacff133fba181
Ladam0203 <adam7605@easv365.dk>
2022-05-11 09:21:33 +0200

    Teacher BE added

---
+++ src/fs3/be/Teacher.java
@@ -0,0 +1,28 @@
+package fs3.be;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class Teacher extends User{
+    StringProperty name;
+
+    public Teacher(String username, String password) {
+        super(username, password);
+        this.name = new SimpleStringProperty();
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public StringProperty nameProperty() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    @Override
+    public String toString(){return this.getUsername();}
+}
