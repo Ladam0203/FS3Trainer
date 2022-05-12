a16705230ae0e4eef23ea79dfa6447b93e0a1f3d
RomanMeasv <90683512+RomanMeasv@users.noreply.github.com>
2022-05-12 10:24:14 +0200

    Teachers' Page UI work
    Adding a lot of FXML files
    Also moved adding templates of citizens to correct tab

---
+++ src/fs3/gui/controller/teacher/tabs/TemplatesController.java
@@ -0,0 +1,46 @@
+package fs3.gui.controller.teacher.tabs;
+
+import fs3.be.CitizenTemplate;
+import fs3.be.GeneralInformation;
+import fs3.be.PersonalInformation;
+import fs3.gui.model.CitizenTemplateModel;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TemplatesController implements Initializable {
+    public ListView<CitizenTemplate> ltvCitizenTemplates;
+    public TextField txfTemplateName;
+
+    private CitizenTemplateModel citizenTemplateModel;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            citizenTemplateModel = new CitizenTemplateModel();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        ltvCitizenTemplates.setItems(citizenTemplateModel.getObservableCitizenTemplates());
+    }
+
+    public void handleAddTemplate(ActionEvent event) {
+        String name = txfTemplateName.getText();
+        if (!name.isEmpty()) {
+            CitizenTemplate citizenTemplate = new CitizenTemplate();
+            PersonalInformation personalInformation = new PersonalInformation();
+            GeneralInformation generalInformation = new GeneralInformation();
+            personalInformation.setName(name);
+            citizenTemplate.setPersonalInformation(personalInformation);
+            try {
+                citizenTemplateModel.createCitizenTemplate(citizenTemplate);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
