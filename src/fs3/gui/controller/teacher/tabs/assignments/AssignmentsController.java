bfc90fba2f9f44ff49c3fecf90502416596ecada
RomanMeasv <90683512+RomanMeasv@users.noreply.github.com>
2022-05-18 11:29:16 +0200

    added method for handling selection of citizen (assigment)

--- src/fs3/gui/controller/teacher/tabs/assignments/AssignmentsController.java
+++ src/fs3/gui/controller/teacher/tabs/assignments/AssignmentsController.java
@@ -2,6 +2,7 @@ package fs3.gui.controller.teacher.tabs.assignments;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
 
 public class AssignmentsController {
      @FXML
