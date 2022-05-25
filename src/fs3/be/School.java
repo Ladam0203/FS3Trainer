b8f338ae23c85db455e1a7a27caf062390dc98a3
Ladam0203 <adam7605@easv365.dk>
2022-05-25 10:27:37 +0200

    ReadAll school DAO and right school BE

--- src/fs3/be/School.java
+++ src/fs3/be/School.java
@@ -1,14 +1,29 @@
 package fs3.be;
 
+import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class School {
+    private IntegerProperty id;
     private StringProperty name;
 
-    public School(String username, String password) {
+    public School() {
         this.name = new SimpleStringProperty();
     }
+
+    public int getId() {
+        return id.get();
+    }
+
+    public IntegerProperty idProperty() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id.set(id);
+    }
+
 
     public String getName() {
         return name.get();
