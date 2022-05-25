cfaa274ee0ee02005b056aa1399d0e5176b78f3c
Ladam0203 <adam7605@easv365.dk>
2022-05-25 11:28:09 +0200

    Teacher has a school field

--- src/fs3/be/Teacher.java
+++ src/fs3/be/Teacher.java
@@ -1,14 +1,18 @@
 package fs3.be;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class Teacher extends User{
     StringProperty name;
+    ObjectProperty<School> school;
 
     public Teacher(String username, String password) {
         super(username, password);
         this.name = new SimpleStringProperty();
+        this.school = new SimpleObjectProperty<>();
     }
 
     public String getName() {
@@ -21,6 +25,18 @@ public class Teacher extends User{
 
     public void setName(String name) {
         this.name.set(name);
+    }
+
+    public School getSchool() {
+        return school.get();
+    }
+
+    public ObjectProperty<School> schoolProperty() {
+        return school;
+    }
+
+    public void setSchool(School school) {
+        this.school.set(school);
     }
 
     @Override
