21f2d973eb762b52fc26cdb3ec49c4f8b7061cfe
Miloš Jozek <90444097+BoldHeadBall@users.noreply.github.com>
2022-05-27 11:33:25 +0200

    medicine list in student

--- src/fs3/gui/controller/student/tabs/MedicineListController.java
+++ src/fs3/gui/controller/student/tabs/MedicineListController.java
@@ -1,5 +1,8 @@
 package fs3.gui.controller.student.tabs;
 
+import fs3.be.CitizenInstance;
+import fs3.gui.model.CitizenInstanceModel;
+import fs3.util.PopUp;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -11,13 +14,27 @@ import java.util.ResourceBundle;
 public class MedicineListController implements Initializable {
     @FXML
     private TextArea txaMedicine;
+    private CitizenInstanceModel citizenInstanceModel;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        try {
+            citizenInstanceModel = CitizenInstanceModel.getInstance();
+        } catch (Exception e) {
+            PopUp.showError("Cannot load citizen instance model", e);
+        }
     }
 
     public void handleSave(ActionEvent event) {
-
+        CitizenInstance selected = citizenInstanceModel.getSelectedCitizenInstance();
+        if(selected == null){
+            return;
+        }
+        selected.setMedicineList(txaMedicine.getText());
+        try {
+            citizenInstanceModel.updateSelectedCitizen();
+        } catch (Exception e) {
+            PopUp.showError("Cannot update citizen!", e);
+        }
     }
 }
